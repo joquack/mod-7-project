7efb997a7b30626b3baf67c9c7c54f70f1b9db61
Joel Cruz <joel.irvin@cruz-family.ws>
2022-08-04 18:13:35 -0700

    server and channel seeders

--- app/seeds/server.py
+++ app/seeds/server.py
@@ -3,11 +3,13 @@ from app.models import db, Server
 
 # Adds a demo user, you can add other users here if you want
 def seed_servers():
-    server1 = Server()
+    server1 = Server(user_id=1, server_name='Test Server', server_img='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png')
+    server2 = Server(user_id=2, server_name='Epic Server')
+    server3 = Server(user_id=2, server_name='Pug Enjoyers', server_img='https://plugins.jetbrains.com/files/7094/91885/icon/pluginIcon.png')
 
-    db.session.add()
-    db.session.add()
-    db.session.add()
+    db.session.add(server1)
+    db.session.add(server2)
+    db.session.add(server3)
 
     db.session.commit()
 
@@ -17,6 +19,6 @@ def seed_servers():
 # TRUNCATE Removes all the data from the table, and RESET IDENTITY
 # resets the auto incrementing primary key, CASCADE deletes any
 # dependent entities
-def undo_users():
-    db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
+def undo_servers():
+    db.session.execute('TRUNCATE servers RESTART IDENTITY CASCADE;')
     db.session.commit()
