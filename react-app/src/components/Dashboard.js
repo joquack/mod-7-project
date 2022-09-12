45fc166ae7b7b3cb5e89abc5c25b3726ca2ce9ee
Joel Cruz <joel.irvin@cruz-family.ws>
2022-09-11 22:49:14 -0700

    dashboard content

--- react-app/src/components/Dashboard.js
+++ react-app/src/components/Dashboard.js
@@ -24,14 +24,44 @@ function Dashboard() {
                 <ServerList />
             </div>
 
-            <div className='d-nav'>
-                <div className='d-nav-top'>
-                    <h1>Welcome to your dashboard, {user.username}!</h1>
-                    <div>
-                        <LogoutButton />
+            <div className='dashboard-main'>
+                <div className='d-nav'>
+                    <div className='d-nav-top'>
+                        <h1>Welcome to your dashboard, {user.username}!</h1>
+                        <div>
+                            <LogoutButton />
+                        </div>
                     </div>
+
+                    <div className='d-content'>
+                        <h2 className='get-started'>To get started, click one of the server icons to jump into a server and start chatting.</h2>
+
+                        <div className='user-details'>
+                            <div>
+                                <img className="d-avatar" src="https://archive.org/download/discordprofilepictures/discordblue.png"></img>
+                            </div>
+
+                            <div className='username-email'>
+                                <h2>User: {user.username}</h2>
+                                <h2>Email: {user.email}</h2>
+                            </div>
+
+                        </div>
+
+                        <h2 className='maintenance'>Nothing here for now but more features will be added in a future update</h2>
+                    </div>
+
                 </div>
+                    <div className='dashboard-footer'>
+                        <h4>Wanna learn how this was built? Check out the repo at
+                            <span>
+                                <a href='https://github.com/joquack/mod-7-project' target="_blank" rel="noopener noreferrer">GitHub</a>
+                            </span>
+                        </h4>
+                    </div>
+
             </div>
+
         </div>
         </>
     )
